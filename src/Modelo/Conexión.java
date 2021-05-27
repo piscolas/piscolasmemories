048c3c7e6aa93fca6d070726506f0eac90683bcb
Piscolas <tomas.ibac@gmail.com>
2021-05-27 16:11:34 -0400

    clase conexion

---
+++ src/Modelo/Conexión.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author tomas
+ */
+public class Conexión {
+    
+    private final String bd="Sistema_de_Biblioteca";
+    private final String usuario="root";
+    private final String clave="12345678";
+    private final String url="";
+    
+}
+ 
